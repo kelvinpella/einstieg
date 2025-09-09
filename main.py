2ac86024f50e5e0d5275f8fd97eeaeefc94fbf23
kelvinpella <kelvinpella@gmail.com>
2025-09-09 03:57:31 +0300

    Remove all project files including Python version, main application code, project configuration, and documentation.

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_project():
-    return f"This is einstieg project or should i spell it for you? {str(list('einstieg')).upper()}"
